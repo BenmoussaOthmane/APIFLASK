7058b413fd9d1909641b57b3be813baefe9dbac9
BenmoussaOthmane <benmoussaothmane@iMac-de-BenmoussaOthmane.local>
2019-05-07 18:18:50 +0100

    scrapinge des email de Air India

--- ScrapingS.py
+++ ScrapingS.py
@@ -1,11 +1,32 @@
 import time
 from selenium import webdriver
-
-
+import re
+import json
+# acceder a la path de chrom dirver
 driver = webdriver.Chrome('/Users/benmoussaothmane/Downloads/chromedriver')
 
-driver.get('https://github.com/BenmoussaOthmane')
 
-print(driver.title)
-driver.quit()
+
+
+# acceder a ala  mon sit GIT
+driver.get('http://www.airindia.in/contact-details.htm')
+
+
+doc = driver.page_source
+emails = re.findall(r'[\w\.-]+@[\w\.-]+' , doc)
+listeE = []
+for email in emails:
+    e = {}
+    e["Email"] = email
+    listeE.append(e)
+    with open ('ScrapingE.json' , 'w') as ecrite:
+        ecrite.write(json.dumps(listeE,indent = 4, sort_keys = True))
+    print(email)
+
+
+
+#  aficher le chrom et l nome de cette site sur la finctione .title
+# print(driver.title)
+#  .quit cette finction pour closer cette page aprais la exucutions
+# driver.quit()
 
